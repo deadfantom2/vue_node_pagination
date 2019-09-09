c6d7c6182f5ab70ccc8e0dab7e1101d2ad63a61e
deadfantom2 <cameratest81@gmail.com>
2019-09-09 23:55:46 +0200

    'second'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-md-4" v-for="article in displayedArticles" :key="article._id">
+          <div class="card mb-2 box-shadow post-cards">
+            <img v-bind:src="article.imageUrl" class="card-img-top">
+            <div class="card-body">
+              <h5 class="capitalize">{{article.title}}</h5>
+              <p class="card-text">{{article.description.slice(0, 120)}}...</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--     Loop through the pages array to display each page number       -->
+      <div class="btn-group col-md-2 offset-md-5">
+        <button
+                type="button"
+                class="btn btn-sm btn-outline-secondary"
+                v-if="page != 1"
+                @click="page--"
+        ><<</button>
+        <button
+                type="button"
+                class="btn btn-sm btn-outline-secondary"
+                v-for="pageNumber in pages.slice(page-1, page+5)"
+                @click="page = pageNumber"
+                :key="pageNumber"
+        >{{pageNumber}}</button>
+        <button
+                type="button"
+                @click="page++"
+                v-if="page < pages.length"
+                class="btn btn-sm btn-outline-secondary"
+        >>></button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                articles: [],
+                baseURL: "http://localhost:3000",
+                page: 1,
+                perPage: 9,
+                pages: []
+            };
+        },
+        created() {
+            this.getPosts();
+        },
+        methods: {
+            async getPosts() {
+              const res = await this.axios.get(`${this.baseURL}/articles`);
+              this.articles = res.data.articles;
+            },
+            setArticles() {
+                let numberOfPages = Math.ceil(this.articles.length / this.perPage);
+                console.log(numberOfPages)
+                for (let i = 1; i <= numberOfPages; i++) {
+                    this.pages.push(i);
+                }
+            },
+            paginate(articles) {
+                let page = this.page;
+                let perPage = this.perPage;
+                let from = (page * perPage) - perPage;
+                let to = (page * perPage);
+                console.log("page: "+page, "perPage: "+ perPage, "from: "+from, "to: "+ to)
+                return articles.slice(from, to);
+            }
+        },
+        watch: {
+            articles(){
+                this.setArticles();
+            }
+        },
+        computed: {
+            displayedArticles: function () {
+                return this.paginate(this.articles);
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
